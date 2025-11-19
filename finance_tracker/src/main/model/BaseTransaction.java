f0e1aab6203a8764f409750e3a81648e35a4273b
Sean Omeife <omeifesean@gmail.com>
2025-11-19 16:53:57 +0000

    Created new file 'BaseTransaction'

--- finance_tracker/src/main/model/BaseTransaction.java
+++ finance_tracker/src/main/model/BaseTransaction.java
@@ -12,5 +12,41 @@ import java.util.UUID;
  * IncomeTransaction and ExpenseTransaction to extend it.
  */
 public non-sealed abstract class BaseTransaction implements Entry{
+    protected final String id;
+    protected final String description;    
+    protected final Money amount;
+    protected final Category category;
+    protected final LocalDate date;
+    
+    // this() constructor chaining example
+    public BaseTransaction(Money amount, Category category) {
+        this(UUID.randomUUID().toString(), "No Description", amount, category, LocalDate.now());
+    }
 
+    public BaseTransaction(String id, String description, Money amount, Category category, LocalDate date){
+        this.id = id;
+        this.description = description == null? "" : description;
+        this.amount = amount;
+        this.category = category;
+        this.date = date == null ? LocalDate.now() : date;
+    }
+
+    public String id() {
+        return id;
+    }
+    public String descripton() {
+        return description;
+    }
+    public Money amount() {
+        return amount;
+    }
+    public Category category() {
+        return category;
+    }
+    public LocalDate date() {
+        return date;
+    }
+
+    // abstract method to be overwritten by subclasses (polynorphism)
+    public abstract String summary();
 }
